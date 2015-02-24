312f48b66c8cfcf0d17a8a6614d7a3d77bcafb26
MaharajSanthir <msanthir@gmail.com>
2015-02-24 01:17:32 -0500

    Lab3 for PRO670 Completed

---
+++ Lab3/Lab3/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbx_age = new System.Windows.Forms.TextBox();
+            this.lbl_age = new System.Windows.Forms.Label();
+            this.btn_calculate = new System.Windows.Forms.Button();
+            this.lbl_result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbx_age
+            // 
+            this.tbx_age.Location = new System.Drawing.Point(161, 42);
+            this.tbx_age.Name = "tbx_age";
+            this.tbx_age.Size = new System.Drawing.Size(107, 20);
+            this.tbx_age.TabIndex = 0;
+            this.tbx_age.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbx_age_KeyDown);
+            // 
+            // lbl_age
+            // 
+            this.lbl_age.AutoSize = true;
+            this.lbl_age.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_age.Location = new System.Drawing.Point(23, 40);
+            this.lbl_age.Name = "lbl_age";
+            this.lbl_age.Size = new System.Drawing.Size(132, 20);
+            this.lbl_age.TabIndex = 1;
+            this.lbl_age.Text = "Enter your age:";
+            // 
+            // btn_calculate
+            // 
+            this.btn_calculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_calculate.Location = new System.Drawing.Point(281, 40);
+            this.btn_calculate.Name = "btn_calculate";
+            this.btn_calculate.Size = new System.Drawing.Size(166, 32);
+            this.btn_calculate.TabIndex = 2;
+            this.btn_calculate.Text = "Calculate Birth Year";
+            this.btn_calculate.UseVisualStyleBackColor = true;
+            this.btn_calculate.Click += new System.EventHandler(this.btn_calculate_Click);
+            // 
+            // lbl_result
+            // 
+            this.lbl_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_result.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lbl_result.Location = new System.Drawing.Point(27, 87);
+            this.lbl_result.Name = "lbl_result";
+            this.lbl_result.Size = new System.Drawing.Size(493, 125);
+            this.lbl_result.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 254);
+            this.Controls.Add(this.lbl_result);
+            this.Controls.Add(this.btn_calculate);
+            this.Controls.Add(this.lbl_age);
+            this.Controls.Add(this.tbx_age);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Birth Year Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbx_age;
+        private System.Windows.Forms.Label lbl_age;
+        private System.Windows.Forms.Button btn_calculate;
+        private System.Windows.Forms.Label lbl_result;
+    }
+}
+
---
+++ Lab3/Lab3/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calculate_Click(object sender, EventArgs e)
+        {
+            //Clear lbl_result
+            lbl_result.Text = "";
+
+            #region Initiate: UserInput to hold user entry, AgeEx for user entry validation
+            //************************************************************************************
+            string UserInput = tbx_age.Text;
+            Regex AgeEx = new Regex(@"^\d+$");
+            #endregion
+
+            if (AgeEx.IsMatch(UserInput)) // Check user entry is number. IF yes proceed.
+            {
+                #region Calculate Birthyear: Subtract age value entered from current year. Display Birthyear. 
+                //*******************************************************************************************
+                int Birthyear = DateTime.Now.Year - Convert.ToInt32(UserInput);
+                lbl_result.Text = string.Format("Your birth year is {0}.", Birthyear);
+                #endregion
+
+                #region If the user is born between now and December 31, the year should be minus one.
+                //************************************************************************************
+                // Birthyear will be the previous year except on December 31.
+                if (DateTime.Now.ToString("m") != "31 December")
+                    lbl_result.Text += string.Format("\n\nOR\n\n{0} (If you were born between {1} - 31 December)", Birthyear - 1, DateTime.Now.ToString("m"));
+                else
+                    lbl_result.Text += " \n\n :) Happy Birthday! :)";
+                #endregion
+            }
+            else // Ask user to enter number only.
+                lbl_result.Text = "\n Enter only numbers for age.";
+        }
+
+        #region Code to press Enter in textbox to press calculate button without mouse click
+        //**************************************************************************
+        private void tbx_age_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                btn_calculate.PerformClick();
+        }
+        #endregion
+
+    }
+}
